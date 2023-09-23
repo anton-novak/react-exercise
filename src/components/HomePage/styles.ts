37a98fe1157c36649b958b893e7b78edbf635993
Anton Novak <anton-novak@outlook.com>
2023-09-23 16:47:36 +0100

    feat: use controlled form with validation

---
+++ src/components/HomePage/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components"
+import { Button } from "@cruk/cruk-react-components"
+
+const StyledButton = styled(Button)`
+	margin-top: 1em;
+	margin-bottom: 1em;
+`
+export default StyledButton;
